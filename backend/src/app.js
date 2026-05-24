f2d6d14e22c35b0a3d6d4d390f66f7add58c3301
hacker444-coder <ronishtamang631@gmail.com>
2026-05-24 09:53:37 +0545

    change url of frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization','cookie']
+  allowedHeaders: ['Content-Type', 'Authorization','cookie','set-cookie'],
 }));
 
 // Middleware
