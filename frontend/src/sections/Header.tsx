ca8c2bfb2b762e989ad2e58c13b14bc1c2616668
hacker444-coder <ronishtamang631@gmail.com>
2026-05-26 07:55:03 +0545

    changes design for menu

--- frontend/src/sections/Header.tsx
+++ frontend/src/sections/Header.tsx
@@ -19,12 +19,12 @@ function Header() {
 
       {isOpen && (
         <div className="absolute top-16 right-0 w-40 rounded-md flex font-bold flex-col px-5 py-5">
-          <a href="#hero" className=" bg-orange-600 rounded-2xl  mb-2">Home</a>
-          <a href="#skills" className=" bg-orange-600 rounded-2xl mb-2">Skill</a>
-          <a href="#projects" className="bg-orange-600 rounded-2xl mb-2">Project</a>
-          <a href="#about" className="bg-orange-600 rounded-2xl mb-2 ">About</a>
-          <a href="#reviews" className="bg-orange-600 rounded-2xl mb-2 ">Review</a>
-          <a href="#footer" className="bg-orange-600 rounded-2xl mb-2 ">Contact</a>
+          <a href="#hero" className=" bg-orange-600 text-center rounded-2xl  mb-2">Home</a>
+          <a href="#skills" className=" bg-orange-600 text-center rounded-2xl mb-2">Skill</a>
+          <a href="#projects" className="bg-orange-600 text-center rounded-2xl mb-2">Project</a>
+          <a href="#about" className="bg-orange-600 text-center rounded-2xl mb-2 ">About</a>
+          <a href="#reviews" className="bg-orange-600 text-center rounded-2xl mb-2 ">Review</a>
+          <a href="#footer" className="bg-orange-600 text-center rounded-2xl mb-2 ">Contact</a>
         </div>
       )}
 
